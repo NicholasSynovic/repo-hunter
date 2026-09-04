61dfce8c6a3c93a61893542d8a9d15f60595d28f
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2026-09-04 14:34:02 -0500

    Migrate code into their respective files and integrate the CLI into main.py

--- joss/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Allow running the JOSS toolkit via ``python -m joss``."""
-
-from joss.main import main
-
-main()
--- joss/main.py
+++ joss/main.py
@@ -2,21 +2,18 @@
 
 # Copyright (c) 2025 Nicholas M. Synovic
 
+import os
 import sys
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
 
-from joss.cli import APPLICATION_NAME, CLI
+from joss import APPLICATION_NAME
+from joss.cli import APPLICATION_NAME
 from joss.db import DB
-from joss.logger import JOSSLogger
 from joss.runner import JOSSRunner
 
 
-def main() -> int:
-    """Entry point for the JOSS CLI application."""
-    args = CLI().run()
-
-    logger: JOSSLogger = JOSSLogger(name=__name__)
-    logger.setup_file_logging(prefix=APPLICATION_NAME)
-
+def main(args: Namespace) -> int:
     resolve_urls: bool = args.resolve_urls
     db: DB = DB(joss_logger=logger, db_path=args.out_file)
     JOSSRunner(joss_logger=logger, db=db, resolve_urls=resolve_urls).run()
@@ -25,4 +22,32 @@ def main() -> int:
 
 
 if __name__ == "__main__":
-    main()
+    github_token: str | None = os.getenv("GITHUB_TOKEN")
+
+    parser: ArgumentParser = ArgumentParser(
+        prog=APPLICATION_NAME,
+        description="",
+        epilog="Created by Nicholas M. Synovic",
+    )
+    parser.add_argument(
+        "-g",
+        "--github-token",
+        default=github_token,
+        required=github_token is None,
+        help="GitHub personal access token",
+    )
+    parser.add_argument(
+        "-o",
+        "--out-file",
+        required=True,
+        help="SQLite3 database path to write to.",
+        type=lambda x: Path(x).absolute(),
+    )
+    parser.add_argument(
+        "--resolve-urls",
+        required=False,
+        help="Resolve URLs (can take a while)",
+        action="store_true",
+    )
+
+    main(args=parser.parse_args())
